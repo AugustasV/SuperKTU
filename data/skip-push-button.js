1acf774ea99ae663a40896990e985fe6a5c7d165
Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
2015-02-18 00:32:53 +0200

    Remove redundant options, fix bugs

--- data/skip-push-button.js
+++ data/skip-push-button.js
@@ -1,5 +1,6 @@
-if (self.options.status == "S") {
+self.port.on("data", function (d) {
     var el = document.querySelector('button[class="btn"]');
     if (el)
-	el.click();
-}
+	if (d[0] == "S" && d[1] == true)
+	    el.click();
+});
