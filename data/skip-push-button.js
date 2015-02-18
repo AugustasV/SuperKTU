8483741564bcf18f003d1684c43483bec00dc3a4
Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
2015-02-18 18:41:15 +0200

    Improve coding style

--- data/skip-push-button.js
+++ data/skip-push-button.js
@@ -1,23 +1,21 @@
 self.port.on("data", function (d) {
     if (window.location.href == "https://pastas.ktu.lt/") {
-	if (d[1] == true) {
+	if (d[1]) {
+	    var el;
 	    switch (d[0]) {
 	    case "S":
-		var el = document.querySelector('input[class="art-button"]');
-		if (el)
-		    el.click();
+		el = document.querySelector('input[class="art-button"]');
 		break;
 	    case "O":
-		var el = document.querySelector('a[class="art-button"]');
-		if (el)
-		    el.click();
+		el = document.querySelector('a[class="art-button"]');
 		break;
 	    }
+	    if (el)
+		el.click();
 	}
     } else if (window.location.href == "https://uais.cr.ktu.lt/ktuis/stp_prisijungimas") {
 	var el = document.querySelector('button[class="btn"]');
-	if (el)
-	    if (d[0] == "S" && d[1] == true)
+	if (el && d[0] == "S" && d[1])
 		el.click();
     }
 });
