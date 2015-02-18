4adfdd1e70ca8de58fb7d83789e04a301456d209
Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
2015-02-18 11:34:06 +0200

    Add pastas.ktu.lt skipping

--- data/skip-push-button.js
+++ data/skip-push-button.js
@@ -1,6 +1,23 @@
 self.port.on("data", function (d) {
-    var el = document.querySelector('button[class="btn"]');
-    if (el)
-	if (d[0] == "S" && d[1] == true)
-	    el.click();
+    if (window.location.href == "https://pastas.ktu.lt/") {
+	if (d[1] == true) {
+	    switch (d[0]) {
+	    case "S":
+		var el = document.querySelector('input[class="art-button"]');
+		if (el)
+		    el.click();
+		break;
+	    case "O":
+		var el = document.querySelector('a[class="art-button"]');
+		if (el)
+		    el.click();
+		break;
+	    }
+	}
+    } else if (window.location.href == "https://uais.cr.ktu.lt/ktuis/stp_prisijungimas") {
+	var el = document.querySelector('button[class="btn"]');
+	if (el)
+	    if (d[0] == "S" && d[1] == true)
+		el.click();
+    }
 });
