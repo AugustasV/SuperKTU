89887e596611b46fd36b9d30c6ac9d4dcbd4db23
Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
2015-02-18 10:59:08 +0200

    Remove the data file responsible for hiding the skip option

--- data/skip-set.js
+++
@@ -1,8 +0,0 @@
-self.port.on("status", function(st) {
-    var el = document.querySelector('setting[pref="extensions.jid1-wb5eyKYVTQKBOQ@jetpack.skip"]');
-    if (el)
-	if (st == "S")
-	    el.style.display = '';
-	else
-	    el.style.display = 'none';
-});
