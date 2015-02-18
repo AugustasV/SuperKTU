5e656c8d5dc09b6a87f7f864e110307f1ef85ebc
Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
2015-02-18 20:07:48 +0200

    Convert the status choice to a boolean (student or not a student)

--- data/skip-push-button.js
+++ data/skip-push-button.js
@@ -3,10 +3,10 @@ self.port.on("data", function (d) {
 	if (d[1]) {
 	    var el;
 	    switch (d[0]) {
-	    case "O":
+	    case false:
 		el = document.querySelector('input[class="art-button"]');
 		break;
-	    case "S":
+	    case true:
 		el = document.querySelector('a[class="art-button"]');
 		break;
 	    }
@@ -15,7 +15,7 @@ self.port.on("data", function (d) {
 	}
     } else if (window.location.href == "https://uais.cr.ktu.lt/ktuis/stp_prisijungimas") {
 	var el = document.querySelector('button[class="btn"]');
-	if (el && d[0] == "S" && d[1])
+	if (el && d[0] && d[1])
 		el.click();
     }
 });
