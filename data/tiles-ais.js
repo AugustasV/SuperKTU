7fc7a4e644e8d3bee63a944cb1d415ebba4ba512
Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
2015-02-26 21:30:52 +0200

    Add the rest of tile managing fns, various fixes
    The number of tiles reduced to 7 (start from 0)
    Change some variable names for clarity
    Prefix fns that are not suitable for public use with _

--- data/tiles-ais.js
+++ data/tiles-ais.js
@@ -1,6 +1,48 @@
 var tiles;
+const numOfTiles = 7;
+const selector = "div[class='span4']";
 
 self.port.on("load", function (data) {
     tiles = data;
 });
+
+function checkIfGood(id) {
+    if (id < 0 || id > numOfTiles)
+	return false;
+    return true;
+}
+
+/* These functions work with page content
+ * Usually they return true on success, false otherwise
+ */
+function _getNthElement(id) {
+    if (!checkIfGood(id))
+	return false;
+    var matchedTiles = document.querySelectorAll(selector);
+    return matchedTiles[id];
+}
+
+function _switchContent(id_a, id_b) {
+    if (!checkIfGood(id_a) || !checkIfGood(id_b))
+	return false;
+
+    var el_a = _getNthElement(id_a), el_b = _getNthElement(id_b);
+    var temp_html = el_b.innerHTML;
+    el_b.innerHTML = el_a.innerHTML;
+    el_a.innerHTML = temp_html;
+    return true;
+}
+
+function _hideTile(id) {
+    if (!checkIfGood(id))
+	return false;
     
+    var tiles = document.querySelectorAll(selector);
+    var visTile = numOfTiles;
+    while (visTile >= 0 && tiles[visTile].style.display &&
+	   tiles[visTile].style.display == "none")
+	visTile--;
+    if (visTile >= 0)
+	tiles[visTile].style.display = "none";
+    return _switchContent(visTile, id);
+}
