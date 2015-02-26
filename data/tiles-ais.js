67810aa2b4bb6f8ef17259b9f73e639ef7972b81
Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
2015-02-27 01:22:58 +0200

    Various improvements
    Now simple switching works and also adds buttons
    Still has a lot of bugs: drops failed messages, doesn't exchange ids in sendMsg() links
    Still doesn't have a button to reset the layout nor does it load the last one on start

--- data/tiles-ais.js
+++ data/tiles-ais.js
@@ -1,6 +1,7 @@
 var tiles;
 const numOfTiles = 7;
 const selector = "div[class='span4']";
+const tSelector = "h4";
 
 self.port.on("load", function (data) {
     tiles = data;
@@ -10,6 +11,94 @@ function checkIfGood(id) {
     if (id < 0 || id > numOfTiles)
 	return false;
     return true;
+}
+
+function sendMsg(id, code) {
+    var ret;
+    window.alert(id + " " + code);
+    switch (code) {
+    case 0:
+	ret = left(id);
+	if (!ret)
+	    window.alert("sendMsg()::left() failed");
+	break;
+    case 1:
+	ret = right(id);
+	if (!ret)
+	    window.alert("sendMsg()::right() failed");
+	break;
+    case 2:
+	ret = up(id);
+	if (!ret)
+	    window.alert("sendMsg()::up() failed");
+	break;
+    case 3:
+	ret = down(id);
+	if (!ret)
+	    window.alert("sendMsg()::down() failed");
+	break;
+    case 4:
+	ret = hide(id);
+	if (!ret)
+	    window.alert("sendMsg()::hide() failed");
+	break;
+    }
+    return ret;
+};
+
+function constructButton(id, code, text) {
+    return "<a onclick='window.sendMsg(" + id + ", " + code + ")'> " + text + "</a>";
+}
+
+exportFunction(sendMsg, unsafeWindow, {defineAs: "sendMsg"});
+
+var titles = document.getElementsByTagName(tSelector);
+for (var i = 0; i <= numOfTiles; i++)
+    // Left Right Down Up Close
+    titles[i].innerHTML += "\
+" + constructButton(i, 0, "&#9668;") + "\
+" + constructButton(i, 1, "&#9658;") + "\
+" + constructButton(i, 2, "&#9660;") + "\
+" + constructButton(i, 3, "&#9650;") + "\
+" + constructButton(i, 4, "&#10005;") + "\
+";
+
+/* These functions change page content and update tiles
+ * Usually these are called when user clicks something
+ */
+function hide(id) {
+    var ret = _hideTile(id);
+    if (ret) {
+	tiles[id].status = false;
+	self.port.emit("load", ss.storage.tiles);
+    }
+}
+
+function left(id) {
+    return switchTiles(id, id-1);
+}
+
+function right(id) {
+    return switchTiles(id, id+1);
+}
+
+function up(id) {
+    return switchTiles(id, id-3);
+}
+
+function down(id) {
+    return switchTiles(id, id+3);
+}
+
+function switchTiles(id_a, id_b) {
+    var ret = _switchContent(id_a, id_b);
+    if (ret) {
+	var temp = tiles[id_a];
+	tiles[id_a] = tiles[id_b];
+	tiles[id_b] = temp;
+	self.port.emit("load", tiles);
+	/* TODO: Exchange sendMsg lines here */
+    }
 }
 
 /* These functions work with page content
