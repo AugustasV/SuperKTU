96739839e21257b6bb6cd16e79a599ed4563ccdc
Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
2015-03-22 15:31:35 +0200

    Move complexity of checking state to lib/skip.js

--- data/skip-push-button.js
+++ data/skip-push-button.js
@@ -1,23 +1,19 @@
 var pastas = 'https://pastas.ktu.lt/';
 var stp = 'https://uais.cr.ktu.lt/ktuis/stp_prisijungimas';
-self.port.on('data', function(d) {
-    if (window.location.href == pastas) {
-        if (d[1]) {
-            var el;
-            switch (d[0]) {
-                case false:
-                    el = document.querySelector('input[class="art-button"]');
-                    break;
-                case true:
-                    el = document.querySelector('a[class="art-button"]');
-                    break;
-            }
-            if (el)
-                el.click();
-        }
-    } else if (window.location.href == stp) {
-        var el = document.querySelector('button[class="btn"]');
-        if (el && d[0] && d[1])
-            el.click();
+if (window.location.href == pastas) {
+    var el;
+    switch (self.options.status) {
+        case false:
+            el = document.querySelector('input[class="art-button"]');
+            break;
+        case true:
+            el = document.querySelector('a[class="art-button"]');
+            break;
     }
-});
+    if (el)
+        el.click();
+} else if (window.location.href == stp) {
+    var el = document.querySelector('button[class="btn"]');
+    if (el && self.options.status)
+        el.click();
+}
