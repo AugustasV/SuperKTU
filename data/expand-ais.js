566f0900d75f2c98d6f627247b8dbbdbef9b2fd9
Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
2015-05-11 21:22:30 +0300

    expand-ais.js: actually use iframe properties for height calculation

--- data/expand-ais.js
+++ data/expand-ais.js
@@ -44,6 +44,7 @@ function createRow(url)
     iframe.onload = function() {
         remove_sp(iframe.contentDocument);
         remove_sl(iframe.contentDocument);
+        iframe.height = iframe.contentWindow.document.body.offsetHeight + 40;
     }
 
     tr.appendChild(td);
